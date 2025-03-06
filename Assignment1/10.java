d9003b95b4d2c60686d915c38d0fcdd97a17b25f
ShrutiKumariSingh <kumarisarita42416@gmail.com>
2025-03-06 13:58:26 +0530

    Create 10.java

---
+++ Assignment1/10.java
@@ -0,0 +1,11 @@
+// Write a java program to Display Alphabets(A to Z)
+
+
+class Alphabets{
+    public static void main(String []args){
+        char c;
+        for(c='A';c<='Z';c++){
+            System.out.print(c+" ");
+        }
+    }
+}
